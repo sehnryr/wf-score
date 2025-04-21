bcebc193efd890474022f9dc8cb2fd2a42ffb423
Youn Mélois <youn@melois.dev>
2025-04-21 22:18:57 +0200

    feat: simple stat editor

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,22 @@
 mod error;
 mod path;
 
+use std::any::TypeId;
+use std::str::FromStr;
+use std::usize;
+
 use dioxus::desktop::{
     Config as DesktopConfig,
     WindowBuilder,
 };
 use dioxus::prelude::*;
+use wf_stats::{
+    Physical,
+    PrimaryElemental,
+    Secondary,
+    SecondaryElemental,
+    Status,
+};
 
 use crate::error::Error;
 use crate::path::app_data_local_dir;
@@ -29,5 +40,193 @@ fn main() -> Result<(), Error> {
 
 #[component]
 fn App() -> Element {
-    rsx! { "Hello, World!" }
+    let mut stats: Signal<Secondary> = use_signal(|| Default::default());
+
+    let critical_chance: Signal<f32> = use_signal(|| Default::default());
+    let critical_damage: Signal<f32> = use_signal(|| Default::default());
+    let status_chance: Signal<f32> = use_signal(|| Default::default());
+    let fire_rate: Signal<f32> = use_signal(|| Default::default());
+    let multishot: Signal<f32> = use_signal(|| Default::default());
+    let ammo_maximum: Signal<usize> = use_signal(|| Default::default());
+    let magazine_size: Signal<usize> = use_signal(|| Default::default());
+    let reload_time: Signal<f32> = use_signal(|| Default::default());
+    let reload_delay: Signal<f32> = use_signal(|| Default::default());
+
+    let mut status_list: Signal<Vec<Status>> = use_signal(|| Default::default());
+
+    use_effect(move || {
+        stats.set(Secondary::new(
+            *critical_chance.read(),
+            *critical_damage.read(),
+            *status_chance.read(),
+            *fire_rate.read(),
+            *multishot.read(),
+            *ammo_maximum.read(),
+            *magazine_size.read(),
+            *reload_time.read(),
+            *reload_delay.read(),
+            status_list.read().clone(),
+        ));
+    });
+
+    rsx! {
+        label { "Critical Chance" }
+        Input { value: critical_chance }
+        br {}
+        label { "Critical Damage" }
+        Input { value: critical_damage }
+        br {}
+        label { "Status Chance" }
+        Input { value: status_chance }
+        br {}
+        label { "Fire Rate" }
+        Input { value: fire_rate }
+        br {}
+        label { "Multishot" }
+        Input { value: multishot }
+        br {}
+        label { "Ammo Maximum" }
+        Input { value: ammo_maximum }
+        br {}
+        label { "Magazine Size" }
+        Input { value: magazine_size }
+        br {}
+        label { "Reload Time" }
+        Input { value: reload_time }
+        br {}
+        label { "Reload Delay" }
+        Input { value: reload_delay }
+        br {}
+        label { "Status List" }
+        button { onclick: move |_| status_list.push(Status::impact(0.0)), "Add Status" }
+        ul {
+            for (i , mut status) in status_list.read().iter().cloned().enumerate() {
+                li {
+                    select {
+                        oninput: move |event| {
+                            let status_type: String = event.value();
+                            let status_damage: f32 = status.damage();
+                            let status = match status_type.as_str() {
+                                "impact" => Status::impact(status_damage),
+                                "puncture" => Status::puncture(status_damage),
+                                "slash" => Status::slash(status_damage),
+                                "cold" => Status::cold(status_damage),
+                                "electricity" => Status::electricity(status_damage),
+                                "heat" => Status::heat(status_damage),
+                                "toxin" => Status::toxin(status_damage),
+                                "blast" => Status::blast(status_damage),
+                                "corrosive" => Status::corrosive(status_damage),
+                                "gas" => Status::gas(status_damage),
+                                "magnetic" => Status::magnetic(status_damage),
+                                "radiation" => Status::radiation(status_damage),
+                                "viral" => Status::viral(status_damage),
+                                _ => return,
+                            };
+                            status_list.write()[i] = status;
+                        },
+                        option { disabled: true, "Physical" }
+                        option {
+                            selected: matches!(status, Status::Physical(Physical::Impact(_))),
+                            value: "impact",
+                            "Impact"
+                        }
+                        option {
+                            selected: matches!(status, Status::Physical(Physical::Puncture(_))),
+                            value: "puncture",
+                            "Puncture"
+                        }
+                        option {
+                            selected: matches!(status, Status::Physical(Physical::Slash(_))),
+                            value: "slash",
+                            "Slash"
+                        }
+                        option { disabled: true, "Primary Elemental" }
+                        option {
+                            selected: matches!(status, Status::PrimaryElemental(PrimaryElemental::Cold(_))),
+                            value: "cold",
+                            "Cold"
+                        }
+                        option {
+                            selected: matches!(status, Status::PrimaryElemental(PrimaryElemental::Electricity(_))),
+                            value: "electricity",
+                            "Electricity"
+                        }
+                        option {
+                            selected: matches!(status, Status::PrimaryElemental(PrimaryElemental::Heat(_))),
+                            value: "heat",
+                            "Heat"
+                        }
+                        option {
+                            selected: matches!(status, Status::PrimaryElemental(PrimaryElemental::Toxin(_))),
+                            value: "toxin",
+                            "Toxin"
+                        }
+                        option { disabled: true, "Secondary Elemental" }
+                        option {
+                            selected: matches!(status, Status::SecondaryElemental(SecondaryElemental::Blast(_))),
+                            value: "blast",
+                            "Blast"
+                        }
+                        option {
+                            selected: matches!(status, Status::SecondaryElemental(SecondaryElemental::Corrosive(_))),
+                            value: "corrosive",
+                            "Corrosive"
+                        }
+                        option {
+                            selected: matches!(status, Status::SecondaryElemental(SecondaryElemental::Gas(_))),
+                            value: "gas",
+                            "Gas"
+                        }
+                        option {
+                            selected: matches!(status, Status::SecondaryElemental(SecondaryElemental::Magnetic(_))),
+                            value: "magnetic",
+                            "Magnetic"
+                        }
+                        option {
+                            selected: matches!(status, Status::SecondaryElemental(SecondaryElemental::Radiation(_))),
+                            value: "radiation",
+                            "Radiation"
+                        }
+                        option {
+                            selected: matches!(status, Status::SecondaryElemental(SecondaryElemental::Viral(_))),
+                            value: "viral",
+                            "Viral"
+                        }
+                    }
+                    input {
+                        onchange: move |event| {
+                            let status_damage: f32 = event.value().parse().unwrap_or_default();
+                            status.set_damage(status_damage);
+                            status_list.write()[i] = status;
+                        },
+                        r#type: "number",
+                        placeholder: "0.00",
+                        step: "0.01",
+                    }
+                    button {
+                        onclick: move |_| {
+                            status_list.remove(i);
+                        },
+                        r#type: "button",
+                        "Delete"
+                    }
+                }
+            }
+        }
+        div { "{stats:#?}" }
+    }
+}
+
+#[component]
+fn Input<T: FromStr + Default + 'static>(mut value: Signal<T>) -> Element {
+    rsx! {
+        input {
+            oninput: move |event| value.set(event.value().parse().unwrap_or_default()),
+            r#type: "number",
+            placeholder: if TypeId::of::<T>() == TypeId::of::<f32>() { "0.00" } else if TypeId::of::<T>() == TypeId::of::<usize>() { "0" },
+            step: if TypeId::of::<T>() == TypeId::of::<f32>() { "0.01" } else if TypeId::of::<T>() == TypeId::of::<usize>() { "1" },
+            min: if TypeId::of::<T>() == TypeId::of::<f32>() { "-1000.0" } else if TypeId::of::<T>() == TypeId::of::<usize>() { "0" },
+            max: if TypeId::of::<T>() == TypeId::of::<f32>() { "1000.0" } else if TypeId::of::<T>() == TypeId::of::<usize>() { "{usize::MAX}" },
+        }
+    }
 }
